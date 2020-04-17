ea84712e81011f61a70e45d29dfc6d1d2c4adb7d
Jason Ritenour <jason.ritenour@gmail.com>
2020-04-17 12:13:07 -0400

    Update vmware_inventory.py

--- vmware_inventory.py
+++ vmware_inventory.py
@@ -7,23 +7,7 @@
 # Requirements
 #   - pyvmomi >= 6.0.0.2016.4
 
-# TODO:
-#   * more jq examples
-#   * optional folder hierarchy
 
-"""
-$ jq '._meta.hostvars[].config' data.json | head
-{
-  "alternateguestname": "",
-  "instanceuuid": "5035a5cd-b8e8-d717-e133-2d383eb0d675",
-  "memoryhotaddenabled": false,
-  "guestfullname": "Red Hat Enterprise Linux 7 (64-bit)",
-  "changeversion": "2016-05-16T18:43:14.977925Z",
-  "uuid": "4235fc97-5ddb-7a17-193b-9a3ac97dc7b4",
-  "cpuhotremoveenabled": false,
-  "vpmcenabled": false,
-  "firmware": "bios",
-"""
 
 from __future__ import print_function
 
